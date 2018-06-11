d0e3b9de0f90e751c5e7313f4bcbc29c0763f346
potsane <potsanemohale@gmail.com>
2018-06-12 01:34:47 +0200

    adding list adapotor and view holder

---
+++ app/src/main/java/com/baracoin/contactsmanager/view/list/ContactViewHolder.kt
@@ -0,0 +1,12 @@
+package com.baracoin.contactsmanager.view.list
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+/**
+ * Created by PMohale on 2018/06/12.
+ */
+class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    
+}
---
+++ app/src/main/java/com/baracoin/contactsmanager/view/list/ContactsListAdaptor.kt
@@ -0,0 +1,7 @@
+package com.baracoin.contactsmanager.view.list
+
+/**
+ * Created by PMohale on 2018/06/12.
+ */
+class ContactsListAdaptor  {
+}
